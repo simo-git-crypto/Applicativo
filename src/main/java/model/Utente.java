b240cf6addbf0a6d639e7bfd43716a9cac142747
simo-git-crypto <si.mazzarelli@studenti.unina.it>
2025-04-24 19:48:50 +0200

    Update Utente.java

--- src/main/java/model/Utente.java
+++ src/main/java/model/Utente.java
@@ -1,29 +1,51 @@
-package model;
+package org.example;
+import java.util.Scanner;
+import org.example.ToDo;
+import org.example.Bacheca;
+import org.example.Condivisione;
+import org.example.ToDo_BACHECA;
 
-/**
- * The type Utente.
- */
 public class Utente {
     private final String login;
     private String password;
-
-    /**
-     * Instantiates a new Utente.
-     *
-     * @param login    the login
-     * @param password the password
-     */
-    public Utente(String login, String password) {
+    public Utente(String login, String password){
         this.login = login;
-        this.password = password;
+    this.password = password;
     }
+    public static Utente creaDaTastiera(){
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Inserire login : ");
+        String login=sc.nextLine();
+        System.out.println("Inserire password: ");
+        String password=sc.nextLine();
 
-    /**
-     * Gets login.
-     *
-     * @return the login
-     */
-    public String getLogin() {
+        return new Utente(login, password);
+    }
+    public boolean ModificaPassword(String vecchiapassword, String nuovapassword){
+        if (vecchiapassword == null || nuovapassword == null || vecchiapassword.isEmpty() || nuovapassword.isEmpty()) {
+            System.out.println(" Errore: Le password non possono essere vuote.");
+            return false;
+        }
+        if(this.password.equals(vecchiapassword)){
+            this.password=nuovapassword;
+            System.out.println("Password modificata correttamente !");
+            return true; }
+        System.out.println("La password inserita non è corretta !");
+        return false; }
+    private boolean attivo=true;
+    public void eliminaUtente(){
+        if(!attivo){
+            System.out.println("Utente non esistente");
+            return;
+        }
+        attivo=false;
+        System.out.println("Utente eliminato");
+    }
+    public boolean isAttivo(){
+        return attivo; }
+    public String getLogin(){
         return login;
+    } public String getPassword(){
+        return password;
     }
 }
